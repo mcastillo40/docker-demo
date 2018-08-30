1b49cb313b2ec6d0ea44c9a073917c6c70ac9d21
Matthew Castillo <chgreen08@gmail.com>
2018-08-29 22:15:36 -0700

    Add docker files

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Connect to MongoDB
+// Connect to MongoDB through mongo container
 mongoose
   .connect(
     'mongodb://mongo:27017/docker-node-mongo',
